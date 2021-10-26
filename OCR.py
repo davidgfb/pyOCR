2ec28c0e14891c43620eaae71310a4cd6b320bde
davidgfb <davidgracia99@gmail.com>
2021-10-26 21:29:49 +0200

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -1,23 +1,32 @@
+from numpy import array
+from pytesseract import image_to_string
+from cv2 import cvtColor, imshow
+from PIL.ImageGrab import grab
 from win32.win32gui import EnumWindows, GetWindowRect,\
      GetWindowText, FindWindow, SetForegroundWindow, ShowWindow,\
      GetWindowPlacement
-from PIL.ImageGrab import grab
 from time import sleep
-
-topwindows = []
 
 def callback(hwnd, extra):
     topwindows.append(GetWindowText(hwnd).lower()) 
 
 def index_containing_substring(the_list, substring):
-    for i, s in enumerate(the_list):
-        if substring in s:
-              return i
+    res = 0
+    
+    for pos in range(len(the_list)):        
+        if substring in the_list[pos]:
+            res = pos
+            
+    return res
+
+topwindows = []
 
 EnumWindows(callback, topwindows)
 
-if any("chrome" in mystring for mystring in topwindows):
-    indice = index_containing_substring(topwindows, "chrome")
+proceso = "chrome"
+
+if any(proceso in mystring for mystring in topwindows):
+    indice = index_containing_substring(topwindows, proceso)
     hwnd = FindWindow(None, topwindows[indice])
 
     if GetWindowPlacement(hwnd)[1] == 2: # estaMinimizada
@@ -26,9 +35,12 @@ if any("chrome" in mystring for mystring in topwindows):
     rect = GetWindowRect(hwnd)[:4]       
 
     SetForegroundWindow(hwnd)
-    sleep(0.2)
+    sleep(0.2) #while not GetWindowPlacement(hwnd)[1] == 1:
 
-    a = grab(bbox = rect)
-    a.show()
-    
+    a = cvtColor(array(grab(bbox = rect)), 6) #a.show()
+    imshow(None, a) 
+
+    tesstr = image_to_string(a, lang ='eng')
+
+    print(tesstr)
 
